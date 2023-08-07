3fbeb752795959e6483c97d2fd1b9888070bbab1
Phil Rice <phil@philrice.uk>
2023-08-07 18:00:56 +0100

    Update variables.tf

--- 101-terraform-job/terraform/variables.tf
+++ 101-terraform-job/terraform/variables.tf
@@ -1,7 +1,7 @@
 variable "appname" {
   type = string
   description = "Application name. Use only lowercase letters and numbers"
-  default = "starterterraform"
+  default = "pdrdevopsterraform"
 }
 
 variable "environment" {
@@ -13,11 +13,11 @@ variable "environment" {
 variable "location" {
   type    = string
   description = "Azure region where to create resources."
-  default = "North Europe"
+  default = "UK South"
 }
 
 variable "department" {
   type    = string
   description = "A sample variable passed from the build pipeline and used to tag resources."
-  default = "Engineering"
+  default = "Platform Engineering"
 }
